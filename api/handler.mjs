b4f6ab512b873226a80917ff329359a54275678e
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-20 13:49:54 -0700

    Fix handler, although we are not calling it yet

--- api/handler.mjs
+++ api/handler.mjs
@@ -1,34 +1,19 @@
-// import httpProxy from 'http-proxy';
-// import http from 'node:http';
+import httpProxy from 'http-proxy';
 
-// if (!process.env.OPENAI_API_KEY) {
-//   throw new Error('OPENAI_API_KEY environment variable must be set');
-// }
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('OPENAI_API_KEY environment variable must be set');
+}
 
-// const proxy = httpProxy.createProxy({ target: 'https://api.openai.com', changeOrigin: true, selfHandleResponse: true });
-// proxy.on('proxyRes', (proxyRes, _req, res) => {
-//   proxyRes.on('data', res.write.bind(res));
-//   proxyRes.on('end', res.end.bind(res));
-// });
-// const server = http.createServer(function (req, res) {
-//   req.headers.authorization = `Bearer ${process.env.OPENAI_API_KEY}`;
-//   proxy.web(req, res);
-// });
+const proxy = httpProxy.createProxy({ target: 'https://api.openai.com', changeOrigin: true, selfHandleResponse: true });
+proxy.on('proxyRes', (proxyRes, _req, res) => {
+  proxyRes.on('data', res.write.bind(res));
+  proxyRes.on('end', res.end.bind(res));
+});
 
-// server.listen(4000, () => {
-//   console.log('Server listening on port 4000');
-// });
-
-
-// import { VercelRequest, VercelResponse } from '@vercel/node';
- 
 export default function handler(
-  request,
-  response,
+  req,
+  res,
 ) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+  req.headers.authorization = `Bearer ${process.env.OPENAI_API_KEY}`;
+  proxy.web(req, res);
 }
