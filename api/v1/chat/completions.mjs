5264264f3d51023b1284436eb9b319660b61d81e
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-20 14:08:36 -0700

    Try a different proxy approach, but they both fail with the same ERR_STREAM_WRITE_AFTER_END error

--- api/v1/chat/completions.mjs
+++ api/v1/chat/completions.mjs
@@ -1,19 +1,35 @@
-import httpProxy from 'http-proxy';
+// import httpProxy from 'http-proxy';
 
-if (!process.env.OPENAI_API_KEY) {
-  throw new Error('OPENAI_API_KEY environment variable must be set');
-}
+// if (!process.env.OPENAI_API_KEY) {
+//   throw new Error('OPENAI_API_KEY environment variable must be set');
+// }
 
-const proxy = httpProxy.createProxy({ target: 'https://api.openai.com', changeOrigin: true, selfHandleResponse: true });
-proxy.on('proxyRes', (proxyRes, _req, res) => {
-  proxyRes.on('data', res.write.bind(res));
-  proxyRes.on('end', res.end.bind(res));
+// const proxy = httpProxy.createProxy({ target: 'https://api.openai.com', changeOrigin: true, selfHandleResponse: true });
+// proxy.on('proxyRes', (proxyRes, _req, res) => {
+//   // proxyRes.on('data', res.write.bind(res));
+//   // proxyRes.on('end', res.end.bind(res));
+// });
+
+// export default function handler(
+//   req,
+//   res,
+// ) {
+//   req.headers.authorization = `Bearer ${process.env.OPENAI_API_KEY}`;
+//   proxy.web(req, res);
+// }
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const apiProxy = createProxyMiddleware({
+  target: 'https://api.openai.com',
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api": "" // strip "/api" from the URL
+  },
+  onProxyRes(proxyRes) {
+  }
 });
 
-export default function handler(
-  req,
-  res,
-) {
+export default function handler(req, res) {
   req.headers.authorization = `Bearer ${process.env.OPENAI_API_KEY}`;
-  proxy.web(req, res);
-}
+  return apiProxy(req, res);
+};
