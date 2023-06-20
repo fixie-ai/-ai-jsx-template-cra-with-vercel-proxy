f6f5456d05ebbd3ca799795ab09fceac2253254b
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-20 14:03:52 -0700

    Vercel is wired up correctly, but now we get a "write after end" error

--- src/ai.tsx
+++ src/ai.tsx
@@ -3,5 +3,5 @@ import * as AI from 'ai-jsx/react';
 import { UseOpenAIProxy } from 'ai-jsx/lib/openai';
 
 export function UseHostOpenAIProxy({ children }: { children: AI.Node }) {
-  return <UseOpenAIProxy basePath="/v1">{children}</UseOpenAIProxy>;
+  return <UseOpenAIProxy basePath="/api/v1">{children}</UseOpenAIProxy>;
 }
