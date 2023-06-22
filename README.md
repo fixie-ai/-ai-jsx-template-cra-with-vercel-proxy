c691870a87e16dd36ef2b805a7769ae32578a3dd
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-22 13:34:58 -0700

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # AI.JSX Vercel Proxy Template
 This template repo uses [AI.JSX](https://github.com/fixie-ai/ai-jsx), the AI application framework for JavaScript.
 
+This template demonstrates the [UI + AI.JSX on the client; API calls on the server](https://docs.ai-jsx.com/guides/architecture#ui--aijsx-on-the-client-api-calls-on-the-server) pattern, using a Vercel edge function to proxy calls to OpenAI.
--- api/v1/chat/completions.js
+++ api/v1/chat/completions.js
@@ -9,51 +9,9 @@ module.exports.POST = async function POST(req) {
   })
 
   const openai = new OpenAIApi(configuration)
-  const openAIResponse = await openai.createChatCompletion({
+
+  return openai.createChatCompletion({
     ...reqBody,
     stream: true
-  });
-  const reader = openAIResponse.body.getReader();
-
-  // This still doesn't work.
-  return new Response(
-    new ReadableStream({
-      async pull(controller) {
-        const { done, value } = await reader.read();
-        console.log('got data', value)
-        controller.enqueue(value);
-        if (done) {
-          controller.close();
-        }
-      }
-  }), {
-    status: 200,
-    headers: { 'Content-Type': 'text/event-stream' },
   })
-
-//   return new Promise((resolve) => {
-
-//   openai.createChatCompletion({
-//     ...reqBody,
-//     stream: true
-//   }).then(async openAIResonse => {
-//     const reader = openAIResonse.body.getReader();
-//     let count = 0;
-//     while (true) {
-//       const { done, value } = await reader.read();
-//       res.write(value);
-//     if (done) {
-//       resolve();
-//       return;
-//     }
-//     console.log(count++, new TextDecoder().decode(value));
-//     }  
-//   })
-// })
-
-
-  // return openai.createChatCompletion({
-  //   ...reqBody,
-  //   stream: true
-  // })
 }
